d7e16ce8c5358a60b2a582d95845f1e302753332
Garbonauta <eduardo.moreno.j@gmail.com>
2017-12-11 15:04:49 -0600

    Removed Scroll to top element. I do not think my users will or use this shortcut. It also takes up screen space on small screens.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,7 +4,6 @@ import TopNav from '../../components/TopNav/TopNav'
 import Footer from '../../components/Footer/Footer'
 import Overlay from '../../components/Overlay/Overlay'
 import MobileMenu from '../../components/MobileMenu/MobileMenu'
-import ScrollToTopContainer from '../ScrollToTop/ScrollToTopContainer'
 import { mainContainer, contentContainer, pageContainer, footerPadding } from './styles.module.css'
 import 'prismjs-okaidia-theme/prism-okaidia.css'
 
@@ -59,7 +58,6 @@ class App extends React.Component {
             <div className={footerPadding}/>
           </div>
           <Footer me={me} languages={languages}/>
-          <ScrollToTopContainer/>
         </div>
         <MobileMenu
           me={me} navElements={navElements} socialHeader={socialHeader}
