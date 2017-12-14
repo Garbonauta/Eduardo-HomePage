0619d0a27e5964d7cd141b09be7b2e91e4ddfe5f
Garbonauta <eduardo.moreno.j@gmail.com>
2017-12-14 00:52:10 -0600

    Lazy loaded disqus component to save on bandwidth significantly.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactDisqusComments from 'react-disqus-comments'
+import Observer from 'react-intersection-observer'
+import { container } from './styles.module.css'
 
 export default function Comments ({slug, title, url}) {
   return (
-    <ReactDisqusComments
-      shortname={`eduardo-moreno`}
-      identifier={slug}
-      title={title}
-      url={url}/>
+    <Observer triggerOnce={true}>
+      {inView => inView &&
+        <ReactDisqusComments
+          className={container}
+          shortname={`eduardo-moreno`}
+          identifier={slug}
+          title={title}
+          url={url}/>}
+    </Observer>
   )
 }
 Comments.propTypes = {
